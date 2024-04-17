bd58eb5e3334ac4c7b4b8e5a719cd2e61f14c7a8
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-04-17 17:20:54 +0300

    base for handshake

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # CRFP
 
+read:
+
+https://slyd0g.medium.com/scanning-the-entire-internet-unauthenticated-vnc-servers-f4780279d6b1
+https://datatracker.ietf.org/doc/html/rfc6143#page-8
+
 ## License
 crfp is free and open source library. All code in this repository is licensed under
 - MIT License ([LICENSE.md](https://github.com/Maksasj/crfp/blob/master/LICENSE.md) or https://opensource.org/license/mit/)
--- examples/example_0.c
+++ examples/example_0.c
@@ -1,6 +1,6 @@
 #include "crfp/crfp.h"
 
-#define BUFFLEN 1024
+#define BUFFLEN 4096
 
 int main(int argc, char *argv[]){
 
@@ -8,10 +8,30 @@ int main(int argc, char *argv[]){
 
     crfp_client_connect(client, "192.168.1.103", 5900);
     
-    char buffer[BUFFLEN] = { '\0' };
-    recv(client->socket, buffer, BUFFLEN, 0);
-    printf("Server sent: %s\n", buffer);
+    {
+        char buffer[BUFFLEN] = { '\0' };
+        int len = recv(client->socket, buffer, BUFFLEN, 0);
+        printf("Server sent: %d '%s'\n", len, buffer);
+    }
 
+    char* version = "RFB 003.008\n"; 
+    if(send(client->socket, version, strlen(version), 0) == -1) {
+        fprintf(stderr,"ERROR failed to send packet to server\n");
+    }
+
+    {
+        char byte;
+        int len = recv(client->socket, &byte, 1, 0);
+        printf("Security types %d\n", byte);
+
+        for(int i = 0; i < byte; ++i) {
+            char sec;
+            int len = recv(client->socket, &sec, 1, 0);
+            printf("    Security type: %d\n", sec);
+        }
+    }
+
+    
     crfp_client_close(client);
     crfp_free_client(client);
 
