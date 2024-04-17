e2392682d9199716200d4a97145750813b7e6ede
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-04-17 16:24:33 +0300

    basic client api

--- examples/example_0.c
+++ examples/example_0.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include "crfp/crfp.h"
 
-int main() {
-    printf("Poggers !\n");
+#define BUFFLEN 1024
+
+int main(int argc, char *argv[]){
+
+    CRFPClient* client = crfp_new_client();
+
+    crfp_client_connect(client, "192.168.1.103", 5900);
+    
+    char buffer[BUFFLEN] = { '\0' };
+    recv(client->socket, buffer, BUFFLEN, 0);
+    printf("Server sent: %s\n", buffer);
+
+    crfp_client_close(client);
+    crfp_free_client(client);
 
     return 0;
 }
--- src/crfp/crfp.h
+++ src/crfp/crfp.h
@@ -1,6 +1,6 @@
 #ifndef _CRFP_H_
 #define _CRFP_H_
 
-
+#include "crfp_client.h"
 
 #endif
