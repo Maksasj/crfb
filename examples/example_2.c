2c7e54efa05a85444d5e29aea291302c404c7583
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-04-21 11:43:08 +0300

    reversed ingerered tightvnc security type

--- examples/example_2.c
+++ examples/example_2.c
@@ -221,153 +221,186 @@ int main(int argc, char *argv[]){
                 int len = recv(client->socket, buffer, 15, 0);
                 printf("Server sent: %d '%s'\n", len, buffer);
             }
-
-
-            unsigned short numberOfServerMessages;
-            unsigned short numberOfClientMessages;
-            unsigned short numberOfEncodings;
-
-            {
-                int len = recv(client->socket, &numberOfServerMessages, sizeof(numberOfServerMessages), 0);
-                short_to_little(&numberOfServerMessages);
-                printf("Server sent: %d, '%d'\n", len, numberOfServerMessages);
-            }
-            {
-                int len = recv(client->socket, &numberOfClientMessages, sizeof(numberOfClientMessages), 0);
-                short_to_little(&numberOfClientMessages);
-                printf("Server sent: %d, '%d'\n", len, numberOfClientMessages);
-            }
-            {
-                int len = recv(client->socket, &numberOfEncodings, sizeof(numberOfEncodings), 0);
-                short_to_little(&numberOfEncodings);
-                printf("Server sent: %d, '%d'\n", len, numberOfEncodings);
-            }
-            {
-                unsigned short padding = 0;
-                int len = recv(client->socket, &padding, sizeof(padding), 0);
-                short_to_little(&padding);
-                printf("Server sent: %d, '%d'\n", len, padding);
-            }
-
-            typedef struct Capability {
-                signed int code;
-                char vendor[4]; 
-                char signature[8]; 
-            } Capability;
-
-            printf("\nServer messages:\n");
-            for(int i = 0; i < numberOfServerMessages; ++i) {
-                Capability cap;
-
-                int len = recv(client->socket, &cap.code, sizeof(cap.code), 0);
-                len = recv(client->socket, &cap.vendor, sizeof(cap.vendor), 0);
-                len = recv(client->socket, &cap.signature, sizeof(cap.signature), 0);
-
-                int_to_little(&cap.code);
-
-                printf("    Capability\n");
-                printf("        Code: %d\n", cap.code);
-                printf("        Vendor: %.4s\n", cap.vendor);
-                printf("        Signature: %.8s\n", cap.signature);
-            }
-
-
-            printf("\nClient messages:\n");
-            for(int i = 0; i < numberOfClientMessages; ++i) {
-                Capability cap;
-
-                int len = recv(client->socket, &cap.code, sizeof(cap.code), 0);
-                len = recv(client->socket, &cap.vendor, sizeof(cap.vendor), 0);
-                len = recv(client->socket, &cap.signature, sizeof(cap.signature), 0);
-
-                int_to_little(&cap.code);
-
-                printf("    Capability\n");
-                printf("        Code: %d\n", cap.code);
-                printf("        Vendor: %.4s\n", cap.vendor);
-                printf("        Signature: %.8s\n", cap.signature);
-            }
-
-            printf("\nEncodings:\n");
-            for(int i = 0; i < numberOfEncodings; ++i) {
-                Capability cap;
-                
-                int len = recv(client->socket, &cap.code, sizeof(cap.code), 0);
-                len = recv(client->socket, &cap.vendor, sizeof(cap.vendor), 0);
-                len = recv(client->socket, &cap.signature, sizeof(cap.signature), 0);
-
-                int_to_little(&cap.code);
-
-                printf("    Capability\n");
-                printf("        Code: %d\n", cap.code);
-                printf("        Vendor: %.4s\n", cap.vendor);
-                printf("        Signature: %.8s\n", cap.signature);
-            }
         }
     }
 
-    {
-        // Mistery packet
-        char message[40] = {
-            0x02, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x02, 0xff, 0xff, 0xff, 0x21, 0xff, 0xff, 0xff, 0x20, 0xff, 0xff, 0xff, 0x18, 0xff, 0xff, 0xff, 0x11,
-        }; 
-
-        if(send(client->socket, &message, 40, 0) == -1) {
+    /*
+    {   // Security hanshake
+        char type = 0x00000001; 
+        if(send(client->socket, &type, 1, 0) == -1) {
             fprintf(stderr,"ERROR failed to send packet to server\n");
         }
-    }
-
-    {
-        // Mistery packet
-        char message[4] = {
-            0xfc, 0x00, 0x02, 0x01
-        }; 
-
-        if(send(client->socket, &message, 4, MSG_DONTWAIT) == -1) {
-            fprintf(stderr,"ERROR failed to send packet to server\n");
-        }
-    }
-
-    {
-        typedef struct FramebufferUpdateRequest {
-            unsigned char messageType;
-            unsigned char incremental;
-
-            unsigned short xPosition;
-            unsigned short yPosition;
-            unsigned short width;
-            unsigned short height;
-        } FramebufferUpdateRequest;
-
-        FramebufferUpdateRequest req;
-        req.messageType = 3;
-        req.incremental = 0; // Lost
-
-        req.xPosition = 0;
-        req.yPosition = 0;
-        req.width = 4320;
-        req.height = 1350;
-
-        short_to_little(&req.xPosition);
-        short_to_little(&req.yPosition);
-        short_to_little(&req.width);
-        short_to_little(&req.height);
-
-        if(send(client->socket, &req, sizeof(FramebufferUpdateRequest), MSG_DONTWAIT) == -1) {
-            fprintf(stderr,"ERROR failed to send packet to server\n");
-        }
-
-        printf("Send framebuffer update request !\n");
 
         {
-            char buffer[16] = { '\0' };
-            int len = recv(client->socket, buffer, 16, 0);
-            printf("Server sent: %d\n", len);
+            unsigned int word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d '%d'\n", len, word);
+
+            if(len == 0) {
+                printf("LENGTH ZERO");
+            }
+
+            if(word == 0) {
+                printf("    SecurityResult OK\n");
+            } else if(word == 1) {
+                printf("    SecurityResult Failed\n");
+            } else {
+                printf("    Unofficial SecurityResult type\n");
+            }
+        }
+    }
+    */
+    /*
+    {   // Security hanshake
+        char type = 0x00000001; 
+        if(send(client->socket, &type, 1, 0) == -1) {
+            fprintf(stderr,"ERROR failed to send packet to server\n");
+        }
+
+        {
+            unsigned int word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d '%d'\n", len, word);
+
+            if(len == 0) {
+                printf("LENGTH ZERO");
+            }
+
+            if(word == 0) {
+                printf("    SecurityResult OK\n");
+            } else if(word == 1) {
+                printf("    SecurityResult Failed\n");
+            } else {
+                printf("    Unofficial SecurityResult type\n");
+            }
+        }
+    }
+    */
+    /*
+    printf("\n");
+
+    {   
+        {   // ClientInit
+            typedef struct ClientInit {
+                char sharedFlag;
+            } ClientInit;
+
+            ClientInit clientInit;
+            clientInit.sharedFlag = 0;
+
+            if(send(client->socket, &clientInit, sizeof(ClientInit), 0) == -1) {
+                fprintf(stderr,"ERROR failed to send packet to server\n");
+            }
+
+            printf("Send client init !\n");
+        }
+    
+        {
+            unsigned short word = 0;
+            int len = recv(client->socket, &word, 2, 0);
+            short_to_little(&word);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned short word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            short_to_little(&word);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned short word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            short_to_little(&word);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned short word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            short_to_little(&word);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned short word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            short_to_little(&word);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            unsigned char word = 0;
+            int len = recv(client->socket, &word, sizeof(word), 0);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            char buffer[BUFFLEN] = { '\0' };
+            int len = recv(client->socket, buffer, 2, 0);
+            printf("Server sent: %d, '%d' '%d' '%d'\n", len, buffer[0], buffer[1], buffer[2]);
+        }
+
+        {
+            unsigned int word = 0;
+            int len = recv(client->socket, &word, 4, 0);
+            int_to_little(&word);
+            printf("Server sent: %d, '%d'\n", len, word);
+        }
+
+        {
+            char buffer[19] = { '\0' };
+            int len = recv(client->socket, buffer, 18, 0);
+            printf("Server sent: %d '%s'\n", len, buffer);
         }
     }
 
+    printf("\n");
 
-    {
+    while(1) {
         typedef struct FramebufferUpdateRequest {
             unsigned char messageType;
             unsigned char incremental;
@@ -384,48 +417,20 @@ int main(int argc, char *argv[]){
 
         req.xPosition = 0;
         req.yPosition = 0;
-        req.width = 4320;
-        req.height = 1350;
+        req.width = 1920;
+        req.height = 1080;
 
         short_to_little(&req.xPosition);
         short_to_little(&req.yPosition);
         short_to_little(&req.width);
         short_to_little(&req.height);
 
-        if(send(client->socket, &req, sizeof(FramebufferUpdateRequest), MSG_DONTWAIT) == -1) {
+        if(send(client->socket, &req, sizeof(FramebufferUpdateRequest), 0) == -1) {
             fprintf(stderr,"ERROR failed to send packet to server\n");
         }
 
         printf("Send framebuffer update request !\n");
 
-       {
-            size_t size = 4320 * 1350 * 4;
-                unsigned char* buffer = (char*) malloc(size);
-
-                size_t len = 0;
-                while (len < size) {
-                    ssize_t n = recv(client->socket, buffer + len, size - len, 0);
-                    if (n <= 0) {
-                        // error handling here
-                        break;
-                        printf("Errpr !\n");
-                    }
-                    len += n;
-                }
-
-                printf("%ld\n", len);
-
-                for(int x = 0; x < 4320; ++x) {
-                    for(int y = 0; y < 1350; ++y) {
-                        printf("%x ", *(buffer + 4320 * y + x));
-                    }
-                    printf("\n");
-                }
-
-                free(buffer);        
-        }
-
-        /*
         {
             typedef struct FramebufferUpdate {
                 unsigned char messageType;
@@ -472,10 +477,38 @@ int main(int argc, char *argv[]){
                     printf("height: %d\n", rect.height);
                     printf("encodingType: %d\n", rect.encodingType);
                 }
+
+                size_t size = 1920 * 1080;
+                unsigned char* buffer = (char*) malloc(size);
+
+                size_t len = 0;
+                while (len < size) {
+                    ssize_t n = recv(client->socket, buffer + len, size - len, 0);
+                    if (n <= 0) {
+                        // error handling here
+                        break;
+                        printf("Errpr !\n");
+                    }
+                    len += n;
+                }
+
+                printf("%ld\n", len);
+
+                for(int x = 0; x < 1920; ++x) {
+                    for(int y = 0; y < 1080; ++y) {
+                        printf("%x ", *(buffer + 1920 * y + x));
+                    }
+                    printf("\n");
+                }
+
+                free(buffer);        
             }
         }
-        */
+
+        fflush(stdout);
+        sleep(1);
     }
+    */
 
     crfp_client_close(client);
     crfp_free_client(client);
--- src/crfp/crfp_client.h
+++ src/crfp/crfp_client.h
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <netinet/tcp.h>
 
 typedef struct CRFPClient {
     int socket;
